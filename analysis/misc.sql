3b027768623769af927b9f377a06c361350466a4
Antonio Díaz Parra <antonio@genially.com>
2021-06-09 11:48:16 +0000

    document geniallys table

---
+++ analysis/misc.sql
@@ -0,0 +1,6 @@
+/*
+Geniallys públicos activados en Comunidad => Public === true && Reusable === true
+Genially públicos que no están en Comunidad => Public === true && Reusable === false
+Geniallys privados sin contraseña => NoIndex === true && Public === true
+Geniallys privados con contraseña => NoIndex === true && Public === false
+*/
--- models/staging/genially/stg_genially_geniallys.sql
+++ models/staging/genially/stg_genially_geniallys.sql
@@ -1,7 +1,32 @@
 with geniallys as (
     select
-        _id as genially_id
-        
+        _id as genially_id,
+        typegenially as genially_type,
+        typesubscription as subscription_type,        
+        public as is_public,
+        published as is_published,
+        showinsocialprofile as is_in_social_profile,
+        reusable as is_reusable,
+        inspiration as is_inspiration,
+        deleted as is_deleted,
+        noindex as is_not_indexed,
+        name,
+        tags, 
+        description,
+        friendlyurl,
+        idUser as user_id,
+        idanalytics as analytics_id,
+        reusedfrom as reused_from_id,        
+        idfromtemplate as from_template_id,
+        idteam as team_id,
+        idteamtemplate as team_template_id,
+        idfromteamtemplate as from_team_template_id,
+        lastmodified as modified_at,
+        creationtime as created_at,
+        datepublished as published_at,
+        lastview as last_view_at,
+        datedeleted as deleted_at
+   
     from {{ source('genially', 'geniallys') }}
 )
 
