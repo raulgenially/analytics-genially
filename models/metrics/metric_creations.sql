ec7dc5388335eabd8461e8c6054ac3769ddf328b
Antonio Díaz Parra <antonio@genially.com>
2021-06-23 13:13:38 +0000

    emend (again) metrics model

--- models/metrics/metric_creations.sql
+++ models/metrics/metric_creations.sql
@@ -1,5 +1,6 @@
 with geniallys as (
     select * from {{ ref('geniallys') }}
+    where is_current_user = True
 ),
 
 final as (
@@ -9,7 +10,7 @@ final as (
         user_plan as plan,
         user_sector as sector, 
         user_role as role,
-        user_market as market, 
+        user_market as market,
 
         -- Metrics
         count(genially_id) as total_creations,
@@ -17,11 +18,13 @@ final as (
         countif(is_deleted = False and is_published = True) as published_creations,
         countif(is_deleted = False and is_published = True and is_reusable = True) as reusable_creations,
 
+        /*
         count(distinct user_id) as users,
         count(genially_id) / count(distinct user_id) as creations_per_user,
         countif(is_deleted = False) / count(distinct user_id) as active_creations_per_user,
         countif(is_deleted = False and is_published = True) / count(distinct user_id) as published_creations_per_user,
         countif(is_deleted = False and is_published = True and is_reusable = True) / count(distinct user_id) as reusable_creations_per_user
+        */
 
     from geniallys
     where date(created_at) >= date(2019, 1, 1)
