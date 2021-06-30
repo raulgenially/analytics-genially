77a0fe301e2e962ca50892b274cf8349c81ae63a
Antonio Díaz Parra <antonio@genially.com>
2021-06-30 09:42:32 +0000

    set up the skeleton for collaborative source

---
+++ models/sources/genially/src_genially_collaboratives.sql
@@ -0,0 +1,18 @@
+with collaboratives as (
+    select
+        _id as collaborative_id,
+
+        email as user_email, 
+
+        idgenially as genially_id,
+        iduser as user_id,        
+        iduserowner as user_owner_id,
+        idteam as team_id,
+
+        createat as created_at
+
+    from {{ source('genially', 'collaborative') }}
+    where __hevo__marked_deleted = False
+)
+
+select * from collaboratives
