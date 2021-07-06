8f75da4d5f18061ada634ae272f745fae4c6afbc
Antonio Díaz Parra <antonio@genially.com>
2021-07-06 09:47:07 +0000

    implement metrics_templates model

---
+++ models/metrics/metrics_templates.sql
@@ -0,0 +1,28 @@
+with geniallys as (
+    select * from {{ ref('geniallys') }}
+),
+
+final as (
+    select
+        -- Dimensions
+        DATE(created_at) as created_at,
+        user_plan as plan,
+        user_sector as sector, 
+        user_role as role,
+        user_market as market,
+        template_name,
+       
+        -- Metrics
+        count(genially_id) as n_total_creations,
+        countif(is_deleted = False) as n_active_creations,
+        countif(is_deleted = False and is_published = True) as n_published_creations
+
+    from geniallys
+    where date(created_at) >= date(2020, 1, 1)
+        and user_plan != 'Free' 
+        and origin = 'Template'
+    group by 1, 2, 3, 4, 5, 6
+    order by created_at asc
+)
+
+select * from final
