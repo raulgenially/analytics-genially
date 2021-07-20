f1b3ec36e2bd07cd526a98608622e50a297ee473
Antonio Díaz Parra <antonio@genially.com>
2021-07-20 09:11:22 +0000

    #7 - Re-structure user-related models

--- models/marts/core/activated_users.sql
+++ models/marts/core/activated_users.sql
@@ -1,10 +1,14 @@
+{{ config(
+    materialized='view'
+) }}
+
 with users as (
     select * from {{ ref('users') }}
 ),
 
 final as (
     select * from users
-    where {{ define_evangelist_user() }}
+    where {{ define_activated_user() }}
 )
 
 select * from final
--- models/marts/core/active_users.sql
+++ models/marts/core/active_users.sql
@@ -1,3 +1,7 @@
+{{ config(
+    materialized='view'
+) }}
+
 with users as (
     select * from {{ ref('activated_users') }}
 ),
