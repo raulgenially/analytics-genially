80cdbbca39dc4d5d86d021a314cf5579c0781b9c
LuisCebrian <luicebch@gmail.com>
2021-09-07 09:07:37 +0200

    #35 - Add adhoc queries to keep track of them

---
+++ analysis/adhoc/adhoc_user_data_enrichment.sql
@@ -0,0 +1,18 @@
+-- This query is used here:
+-- https://docs.google.com/spreadsheets/d/1I4tDFcdzNtstH8bknZ0HtbGpQNBiTabb5S-QfvZcm8c
+
+with users as (
+    select * from {{ ref('users') }}
+)
+
+select
+    email,
+    DATE(last_access_at) as last_access_at,
+    plan,
+    market,
+    n_total_creations as total_creations
+
+from users
+-- These emails are used as an example. In the google sheet this is read from an input sheet
+where email in ('luiscebrian@genially.com', 'antonio@genially.com', 'raul@genial.ly')
+order by email asc
