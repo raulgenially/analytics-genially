07b03d9cce59b3957c84fed9386ac19653a41954
LuisCebrian <luicebch@gmail.com>
2021-09-14 13:54:58 +0200

    #41 - Add teamtemplate source to the source models

---
+++ models/sources/genially/src_genially_team_template.sql
@@ -0,0 +1,17 @@
+with teamtemplate as (
+    select * from {{ source('genially', 'teamtemplate') }}
+),
+
+final as (
+    select
+        _id as team_template_id,
+
+        idgenially as genially_id,
+        idteam as team_id,
+        idspace as space_id,
+
+    from teamtemplate
+    where __hevo__marked_deleted = false
+)
+
+select * from final
