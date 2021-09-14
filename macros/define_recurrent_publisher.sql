95de960873765c35431f4afaa667935e958030e9
Antonio Díaz Parra <antonio@genially.com>
2021-09-14 11:06:05 +0000

    #25 - Use a default parameter in the definition of recurrent pub.

--- macros/define_recurrent_publisher.sql
+++ macros/define_recurrent_publisher.sql
@@ -1,4 +1,4 @@
-{% macro define_recurrent_publisher() %}
-    {{ define_publisher() }}
+{% macro define_recurrent_publisher(min_published_creations=1) %}
+    {{ define_publisher(min_published_creations) }}
     and date(last_access_at) > date(registered_at)
 {% endmacro %}
