7d6f82d9aeeb36187844b07c9ef165746734dc0c
Antonio Díaz Parra <antonio@genially.com>
2021-09-15 06:48:02 +0000

    #25 - Use jinja variable for not selected category

--- models/sources/genially/src_genially_users.sql
+++ models/sources/genially/src_genially_users.sql
@@ -15,9 +15,9 @@ final as (
         _id as user_id,
 
         {{ map_subscription_code('typesubscription') }} as subscription_plan,
-        coalesce(sector_codes.sector_name, '{{ not_select }}') as sector,
-        coalesce(sector_codes.category, '{{ not_select }}') as sector_category,
-        coalesce(role_codes.role_name, '{{ not_select }}') as role,
+        coalesce(sector_codes.sector_name, '{{ var('not_selected') }}') as sector,
+        coalesce(sector_codes.category, '{{ var('not_selected') }}') as sector_category,
+        coalesce(role_codes.role_name, '{{ var('not_selected') }}') as role,
         username,
         nickname,
         lower(email) as email,
