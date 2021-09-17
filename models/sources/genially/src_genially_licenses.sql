640075b759a3408400db42033bf88a49d75f410d
LuisCebrian <luicebch@gmail.com>
2021-09-17 11:09:04 +0200

    #45 - Remove price and currency from licenses, they are not reliable

--- models/sources/genially/src_genially_licenses.sql
+++ models/sources/genially/src_genially_licenses.sql
@@ -6,23 +6,6 @@ final as (
     select
         _id as license_id,
 
-        -- convert the price from a string to a number. It handles these cases
-        -- ''
-        -- null
-        -- 12
-        -- 12.23
-        -- 12.23€
-        -- 12.23 €
-        -- 59,90
-        cast(
-            regexp_extract(
-                replace(
-                    if(price in ('', 'NaN') or price is null, '0', price), ',', '.'
-                ),
-                r'[\d.]+'
-            ) as float64
-        ) as price,
-        currency,
         status,
         typelicense as license_type,
         comments,
