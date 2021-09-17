8cdbc0d356c849ad80523247e7eb8c49c9aeb963
Antonio Díaz Parra <antonio@genially.com>
2021-09-17 08:29:38 +0000

    #25 - Encapsulate the concept of recurrency

--- macros/define_recurrent_publisher.sql
+++ macros/define_recurrent_publisher.sql
@@ -1,4 +1,4 @@
 {% macro define_recurrent_publisher(min_published_creations=1) %}
     {{ define_publisher(min_published_creations) }}
-    and date(last_access_at) > date(registered_at)
+    and {{ define_recurrency() }}
 {% endmacro %}
