f347d25c4749470798e8ab584a89bfb01315396f
Antonio Díaz Parra <antonio@genially.com>
2021-09-24 09:41:19 +0000

    #49 - Amend aggregations

--- macros/create_broad_role_field.sql
+++ macros/create_broad_role_field.sql
@@ -1,6 +1,6 @@
 {% macro create_broad_role_field(role, broad_sector) %}
     case
-        when {{ role }} = '{{ var('not_selected') }}'
+        when {{ role }} = '{{ var('not_selected') }}' or {{ broad_sector }} = '{{ var('not_selected') }}'
             then '{{ var('not_selected') }}'
         when {{ role }} like 'Student%'
             then 
@@ -11,10 +11,6 @@
                         then 'Student (Higher)'
                     when {{ broad_sector }} = 'Education (Other)'
                         then 'Student (Other)'
-                    when {{ broad_sector }} = 'Corporate'
-                        then 'Professional'
-                    when {{ broad_sector }} = '{{ var('not_selected') }}'
-                        then 'Student (Not-selected sector)'
                     else null
                 end
         else 'Professional'
