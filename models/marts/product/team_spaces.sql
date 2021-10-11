b4b5bde4aa3c3bfff2358890e0d1a6c207101489
Antonio Díaz Parra <antonio@genially.com>
2021-10-11 14:57:20 +0000

    #55 - Enrich teams-related models

--- models/marts/product/team_spaces.sql
+++ models/marts/product/team_spaces.sql
@@ -1,4 +1,4 @@
-with team_spaces as (
+with spaces as (
     select * from {{ ref('src_genially_team_spaces') }}
 ),
 
@@ -10,7 +10,7 @@ geniallys as (
     select * from {{ ref('team_geniallys') }}
 ),
 
-geniallys_in_spaces as (
+geniallys_spaces as (
     select
         space_id,
         countif({{ define_active_creation('geniallys') }}) as n_active_creations
@@ -21,23 +21,24 @@ geniallys_in_spaces as (
 
 final as (
     select
-        team_spaces.team_space_id,
+        spaces.team_space_id,
 
-        team_spaces.name,
+        spaces.name,
         teams.name as team_name,
-        coalesce(geniallys_in_spaces.n_active_creations, 0) as n_active_creations,
+        coalesce(geniallys_spaces.n_active_creations, 0) as n_active_creations,
 
-        team_spaces.is_common,
+        spaces.is_common,
 
-        team_spaces.team_id,
+        spaces.team_id,
+        spaces.owner_id,
 
-        team_spaces.created_at
+        spaces.created_at
 
-    from team_spaces
+    from spaces
     left join teams
-        on team_spaces.team_id = teams.team_id
-    left join geniallys_in_spaces
-        on team_spaces.team_space_id = geniallys_in_spaces.space_id
+        on spaces.team_id = teams.team_id
+    left join geniallys_spaces
+        on spaces.team_space_id = geniallys_spaces.space_id
 )
 
 select * from final
--- models/marts/product/teams.sql
+++ models/marts/product/teams.sql
@@ -3,10 +3,10 @@ with teams as (
 ),
 
 spaces as (
-    select * from {{ ref('src_genially_team_spaces') }}
+    select * from {{ ref('team_spaces') }}
 ),
 
-team_members as (
+members as (
     select * from {{ ref('src_genially_team_members') }}
 ),
 
@@ -14,25 +14,27 @@ geniallys as (
     select * from {{ ref('team_geniallys') }}
 ),
 
-spaces_in_teams as (
+spaces_teams as (
     select
         team_id,
-        count(team_space_id) as n_spaces
+        count(team_space_id) as n_spaces,
+        countif(n_active_creations > 0) as n_spaces_with_active_creations,
+        count(distinct owner_id) as n_space_creators
 
     from spaces
     group by 1
 ),
 
-members_in_teams as (
+members_teams as (
     select
         team_id,
         countif(confirmed_at is not null) as n_members
 
-    from team_members
+    from members
     group by 1
 ),
 
-geniallys_in_teams as (
+geniallys_teams as (
     select
         team_id,
         countif({{ define_active_creation('geniallys') }}) as n_active_creations
@@ -48,9 +50,11 @@ final as (
         teams.team_type_name as plan,
         teams.name,
         teams.seats as n_seats,
-        coalesce(spaces_in_teams.n_spaces, 0) as n_spaces,
-        coalesce(members_in_teams.n_members, 0) as n_members,
-        coalesce(geniallys_in_teams.n_active_creations, 0) as n_active_creations,
+        coalesce(spaces_teams.n_spaces, 0) as n_spaces,
+        coalesce(spaces_teams.n_spaces_with_active_creations, 0) as n_spaces_with_active_creations,
+        coalesce(members_teams.n_members, 0) as n_members,
+        coalesce(spaces_teams.n_space_creators, 0) as n_space_creators,
+        coalesce(geniallys_teams.n_active_creations, 0) as n_active_creations,
 
         if(teams.logo is null, false, true) as has_logo_in_team_tab,
         if(teams.banner is null, false, true) as has_cover_picture_in_team_tab,
@@ -60,12 +64,12 @@ final as (
         teams.created_at
 
     from teams
-    left join spaces_in_teams
-        on teams.team_id = spaces_in_teams.team_id
-    left join members_in_teams
-        on teams.team_id = members_in_teams.team_id
-    left join geniallys_in_teams
-        on teams.team_id = geniallys_in_teams.team_id
+    left join spaces_teams
+        on teams.team_id = spaces_teams.team_id
+    left join members_teams
+        on teams.team_id = members_teams.team_id
+    left join geniallys_teams
+        on teams.team_id = geniallys_teams.team_id
 )
 
 select * from final
