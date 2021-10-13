a2489fa1b8d4121072e2ddd58cd048d6e80edcef
Antonio Díaz Parra <antonio@genially.com>
2021-10-13 06:00:56 +0000

    #49 - Remove unknown variable [skip ci]

--- macros/extract_user_profile.sql
+++ macros/extract_user_profile.sql
@@ -2,12 +2,8 @@
 -- profile_column can be newsector or newrole
 -- profile_name refers to the normalized name of the corresponding profile
     case
-        when {{ profile_column }} is null
+        when {{ profile_column }} is null or {{ profile_name }} is null
             then '{{ var('not_selected') }}'
-        when {{ profile_name }} is null
-            -- then '{{ var('unknown') }}'
-            -- In practice, it doesn't matter, so let's stick to not selected since this simplifies the subsequent logic.
-            then '{{ var('not_selected') }}' 
         else {{ profile_name }}
     end
 {% endmacro %}
