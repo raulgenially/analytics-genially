51298074530f43030b15a3aeaf596241dc761a98
Antonio Díaz Parra <antonio@genially.com>
2021-10-20 14:57:10 +0000

    #65 - Amend model names

--- models/metrics/creator_activity/metrics_creator_activity.sql
+++ models/metrics/creator_activity/metrics_creator_activity.sql
@@ -1,13 +1,14 @@
 {% set partitions_to_replace = [
-  'date_sub(current_date, interval 1 day)'
+  'date_sub(current_date, interval 1 day)',
+  'date_sub(current_date, interval 2 day)'
 ] %}
 
 {{
   config(
-    materialized = 'incremental',
-    incremental_strategy = 'insert_overwrite',
+    materialized='incremental',
+    incremental_strategy='insert_overwrite',
     partition_by={'field': 'date_day', 'data_type': 'date'},
-    partitions = partitions_to_replace
+    partitions=partitions_to_replace
   )
 }}
 
@@ -115,30 +116,30 @@ user_traffic_rolling_status as (
 
 final as (
     select
-        {{ dbt_utils.surrogate_key(['user_traffic_rolling_status.user_id', 'user_traffic_rolling_status.date_day']) }} as id,
-        user_traffic_rolling_status.user_id,
-        user_traffic_rolling_status.first_usage_at,
-        user_traffic_rolling_status.date_day,
-        user_traffic_rolling_status.n_days_since_first_usage,
-        user_traffic_rolling_status.is_active,
-        user_traffic_rolling_status.status,
-        user_traffic_rolling_status.n_days_active_7d,
-        user_traffic_rolling_status.status_7d,
-        user_traffic_rolling_status.n_days_active_28d,
-        user_traffic_rolling_status.status_28d,
-        lag(user_traffic_rolling_status.status_7d, {{ week_days }}) over (
-            partition by user_traffic_rolling_status.user_id
+        {{ dbt_utils.surrogate_key(['user_id', 'date_day']) }} as id,
+        user_id,
+        first_usage_at,
+        date_day,
+        n_days_since_first_usage,
+        is_active,
+        status,
+        n_days_active_7d,
+        status_7d,
+        n_days_active_28d,
+        status_28d,
+        lag(status_7d, {{ week_days }}) over (
+            partition by user_id
             order by n_days_since_first_usage asc
         ) as previous_status_7d,
-        lag(user_traffic_rolling_status.status_28d, {{ month_days }}) over (
-            partition by user_traffic_rolling_status.user_id
+        lag(status_28d, {{ month_days }}) over (
+            partition by user_id
             order by n_days_since_first_usage asc
         ) as previous_status_28d
 
     from user_traffic_rolling_status
     
     {% if is_incremental() %}
-    where user_traffic_rolling_status.date_day >= date_sub(current_date(), interval 1 day)
+    where date_day >= date_sub(current_date(), interval 2 day)
     {% endif %}
 )
 
--- models/metrics/creator_activity/metrics_creator_activity_status_28d_change_rates.sql
+++ models/metrics/creator_activity/metrics_creator_activity_status_28d_change_rates.sql
@@ -1,12 +1,12 @@
 with activity as (
-    select * from {{ ref('creator_activity_by_day') }}
+    select * from {{ ref('metrics_creator_activity') }}
 ),
 
 denominator as (
     select
         date_day,
         previous_status_28d,
-        count(distinct user_id) as users
+        count(user_id) as n_users
 
     from activity
     group by 1, 2
@@ -17,12 +17,12 @@ numerator as (
         date_day,
         previous_status_28d,
         status_28d,
-        count(distinct user_id) as users
+        count(user_id) as n_users
 
     from activity
-    where ((previous_status_28d = 'A' and status_28d = 'A')
-         or (previous_status_28d = 'N' and status_28d = 'A')
-         or (previous_status_28d = 'C' and status_28d = 'A'))
+    where (previous_status_28d = 'Active' and status_28d = 'Active')
+         or (previous_status_28d = 'New' and status_28d = 'Active')
+         or (previous_status_28d = 'Churned' and status_28d = 'Active')
     {{ dbt_utils.group_by(n=3) }}
 ),
 
@@ -31,18 +31,14 @@ final as (
         denominator.date_day,
         numerator.status_28d,
         numerator.previous_status_28d,
-        numerator.users as active_users,
-        denominator.users as all_users,
-        numerator.users / (denominator.users * 1.0) as change_rate
+        numerator.n_users as n_active_returning_users,
+        denominator.n_users as n_total_users,
+        numerator.n_users / denominator.n_users as change_rate
 
     from denominator
     left join numerator
         on denominator.date_day = numerator.date_day
         and denominator.previous_status_28d = numerator.previous_status_28d
-
-    -- Not sure about this one.
-    where numerator.previous_status_28d is not null
-        and numerator.status_28d is not null
 )
 
 select * from final
