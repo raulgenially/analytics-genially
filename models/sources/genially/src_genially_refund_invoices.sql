9e4e5be7e821d829ce88345291fc5d755c72757f
LuisCebrian <luicebch@gmail.com>
2021-11-04 15:15:38 +0100

    #77 - Extract information from data column - Add reference_invoice_number to the documentation - Account for PP-D-% transactions

--- models/sources/genially/src_genially_refund_invoices.sql
+++ models/sources/genially/src_genially_refund_invoices.sql
@@ -2,13 +2,29 @@ with refund_invoices as (
     select * from {{ source('genially', 'refundinvoice') }}
 ),
 
+int_refund_invoices as (
+    select
+        *,
+
+        trim(json_extract_scalar(data, '$.Name')) as payer_name,
+        trim(json_extract_scalar(data, '$.Cif')) as payer_cif,
+        trim(json_extract_scalar(data, '$.Address')) as payer_address,
+        trim(json_extract_scalar(data, '$.Country')) as payer_country,
+
+    from refund_invoices
+),
+
 final as (
     select
         _id as invoice_id,
 
-        data,
         description,
         payeremail as payer_email,
+        payer_name,
+        -- Some cifs/addresses are denoted with '-', '.' or '--'. Nullify them
+        if(regexp_contains(payer_cif, r'\w+'), payer_cif, null) as payer_cif,
+        if(regexp_contains(payer_address, r'\w+'), payer_address, null) as payer_address,
+        payer_country,
         residencecountry as residence_country,
         -- force all totals to be negative
         abs(cast(total as float64)) * -1 as total,
@@ -24,7 +40,7 @@ final as (
 
         dateinvoce as invoiced_at,
 
-    from refund_invoices
+    from int_refund_invoices
     where __hevo__marked_deleted = false
 )
 
