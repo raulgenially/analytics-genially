16fd31ed4f604cc4daaed78ad6ef359e6e37c981
Antonio Díaz Parra <antonio@genially.com>
2021-11-16 09:35:09 +0000

    #85 - Fix tests

--- tests/users/assert_collaborative_fields_should_match.sql
+++ tests/users/assert_collaborative_fields_should_match.sql
@@ -6,18 +6,18 @@ with users as (
 final as (
     select
         user_id,
-        is_collaborator,
-        is_collaborator_of_creation_in_social_profile,
-        is_collaborator_of_creation_visualized_last_90_days,
-        is_collaborator_of_creation_visualized_last_60_days,
-        is_collaborator_of_creation_visualized_last_30_days
+        is_in_collaboration,
+        is_in_collaboration_of_creation_in_social_profile,
+        is_in_collaboration_of_creation_visualized_last_90_days,
+        is_in_collaboration_of_creation_visualized_last_60_days,
+        is_in_collaboration_of_creation_visualized_last_30_days
 
     from users
-    where is_collaborator = false
-        and (is_collaborator_of_creation_in_social_profile = true
-            or is_collaborator_of_creation_visualized_last_90_days = true
-            or is_collaborator_of_creation_visualized_last_60_days = true
-            or is_collaborator_of_creation_visualized_last_30_days = true)
+    where is_in_collaboration = false
+        and (is_in_collaboration_of_creation_in_social_profile = true
+            or is_in_collaboration_of_creation_visualized_last_90_days = true
+            or is_in_collaboration_of_creation_visualized_last_60_days = true
+            or is_in_collaboration_of_creation_visualized_last_30_days = true)
 )
 
 select * from final
