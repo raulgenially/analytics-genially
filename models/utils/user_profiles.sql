3b70cde4e06398110c07ee74e828b8bfc67b712b
LuisCebrian <luicebch@gmail.com>
2021-11-22 11:30:00 +0100

    #81 - Rename role_correspondence and role_mapping in user_profiles model - Add issue number to the TODO message in stg_templates

--- models/utils/user_profiles.sql
+++ models/utils/user_profiles.sql
@@ -6,7 +6,7 @@ sector_codes as (
     select * from {{ ref('sector_codes') }}
 ),
 
-role_mapping as (
+role_correspondence as (
     select * from {{ ref('role_correspondence') }}
 ),
 
@@ -23,17 +23,17 @@ int_user_profile as (
         on role_codes.sector_id = sector_codes.sector_id
 ),
 
-role_correspondence as (
+role_mapping as (
     select
-        role_mapping.*,
+        role_correspondence.*,
         role_codes.role_name as new_role_name,
         role_codes.sector_id as new_sector_id,
         sector_codes.sector_name as new_sector_name,
         sector_codes.agg_sector
 
-    from role_mapping
+    from role_correspondence
     left join role_codes
-        on role_mapping.new_role_id = role_codes.role_id
+        on role_correspondence.new_role_id = role_codes.role_id
     left join sector_codes
         on role_codes.sector_id = sector_codes.sector_id
 ),
@@ -44,15 +44,15 @@ final as (
         int_user_profile.role_name,
         int_user_profile.sector_id,
         int_user_profile.sector_name,
-        ifnull(role_correspondence.new_role_id, int_user_profile.role_id) as new_role_id,
-        ifnull(role_correspondence.new_role_name, int_user_profile.role_name) as new_role_name,
-        ifnull(role_correspondence.new_sector_id, int_user_profile.sector_id) as new_sector_id,
-        ifnull(role_correspondence.new_sector_name, int_user_profile.sector_name) as new_sector_name,
-        ifnull(role_correspondence.agg_sector, int_user_profile.agg_sector) as agg_sector
+        ifnull(role_mapping.new_role_id, int_user_profile.role_id) as new_role_id,
+        ifnull(role_mapping.new_role_name, int_user_profile.role_name) as new_role_name,
+        ifnull(role_mapping.new_sector_id, int_user_profile.sector_id) as new_sector_id,
+        ifnull(role_mapping.new_sector_name, int_user_profile.sector_name) as new_sector_name,
+        ifnull(role_mapping.agg_sector, int_user_profile.agg_sector) as agg_sector
 
     from int_user_profile
-    left join role_correspondence
-        on int_user_profile.role_id = role_correspondence.old_role_id
+    left join role_mapping
+        on int_user_profile.role_id = role_mapping.old_role_id
 )
 
 select * from final
