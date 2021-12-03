b1fe8fe1ec206afbd0d05aa5f6e1cb02cedcfa41
Antonio Díaz Parra <antonio@genially.com>
2021-12-03 12:20:43 +0000

    #95 - Calculate retention cohorts

--- models/metrics/login_activity/metrics_login_activity_retention.sql
+++ models/metrics/login_activity/metrics_login_activity_retention.sql
@@ -6,16 +6,17 @@ with activity as (
 final as (
     select
         -- Dimensions
-        date_day,
+        n_days_since_first_usage,
         {{ place_main_dimension_fields('activity') }}
 
         -- Metrics
+        count(user_id) as n_total_users,
         countif(is_active) as n_daily_active_users,
+        countif(status_7d in ('New', 'Current')) as n_weekly_active_users,
         countif(status_28d in ('New', 'Current')) as n_monthly_active_users
 
     from activity
     {{ dbt_utils.group_by(n=8) }}
-    order by 1 asc
 )
 
 select * from final
