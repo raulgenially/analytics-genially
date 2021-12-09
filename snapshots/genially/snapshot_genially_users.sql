ebaed6f76ad6f7948a45c60a78aababb4e346674
LuisCebrian <luicebch@gmail.com>
2021-12-09 14:57:46 +0100

    #105 - Add users snapshot to dbt - Remove snapshots from the cleanup dataset macro. Snapshots should be removed manually to prevent accidental deletion when runnning in a dev environment

---
+++ snapshots/genially/snapshot_genially_users.sql
@@ -0,0 +1,18 @@
+{% snapshot snapshot_genially_users %}
+
+    {{
+      config(
+        unique_key='_id',
+        strategy='timestamp',
+        updated_at='updated_at',
+        invalidate_hard_deleted = True
+      )
+    }}
+
+    select
+        *,
+        ifnull(lastaccesstime, dateregister) as updated_at
+    from {{ source('genially', 'users') }}
+    where __hevo__marked_deleted = false
+
+{% endsnapshot %}
