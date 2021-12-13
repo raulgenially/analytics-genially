e14c5338a2df08630e53ad7f8b6ee50d6cad5a52
LuisCebrian <luicebch@gmail.com>
2021-12-13 17:31:58 +0100

    #105 - Use __hevo_ingested_at as the reliable timestamp field to check for updates in a record. Rename the field in case we change etl tools to have a consistent name

--- snapshots/genially/snapshot_genially_users.sql
+++ snapshots/genially/snapshot_genially_users.sql
@@ -4,14 +4,14 @@
       config(
         unique_key='_id',
         strategy='timestamp',
-        updated_at='updated_at',
+        updated_at='etl_loaded_at',
         invalidate_hard_deleted = True
       )
     }}
 
     select
         *,
-        ifnull(lastaccesstime, dateregister) as updated_at
+        timestamp_millis(__hevo__ingested_at) as etl_loaded_at
 
     from {{ source('genially', 'users') }}
     where __hevo__marked_deleted = false
