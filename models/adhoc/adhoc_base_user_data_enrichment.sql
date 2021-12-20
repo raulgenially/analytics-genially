2e365f9242d23c62ab5b4bbf0666a98e6c2cb9b7
LuisCebrian <luicebch@gmail.com>
2021-12-20 12:49:54 +0100

    # 111 - Replace references to src_genially_invoices and         src_genially_licenes to their stg versions in adhoc queries

--- models/adhoc/adhoc_base_user_data_enrichment.sql
+++ models/adhoc/adhoc_base_user_data_enrichment.sql
@@ -1,27 +1,34 @@
 with licenses as (
-    select * from {{ ref('src_genially_licenses') }}
+    select * from {{ ref('stg_licenses') }}
 ),
 
 invoices as (
-    select * from {{ ref('src_genially_invoices') }}
+    select * from {{ ref('stg_invoices') }}
+    where is_refund = false
 ),
 
 users as (
     select * from {{ ref('users') }}
 ),
 
-numbered_licenses as (
-    select
-        *,
-        row_number() over(partition by user_id order by started_at desc) as rn
-    from licenses
+last_licenses as (
+   {{ unique_records_by_column(
+        'licenses',
+        'user_id',
+        order_by='started_at',
+        dir='desc',
+      )
+   }}
 ),
 
-numbered_invoices as (
-    select
-        *,
-        row_number() over(partition by subscription_id order by invoiced_at desc) as rn
-    from invoices
+last_invoices as (
+   {{ unique_records_by_column(
+        'invoices',
+        'subscription_id',
+        order_by='invoiced_at',
+        dir='desc',
+      )
+   }}
 ),
 
 final as (
@@ -33,23 +40,17 @@ final as (
         users.sector as Sector,
         users.country as Country,
         date(users.last_access_at) as LastAccessTime,
-        numbered_licenses.license_type as LicenseType,
-        date(numbered_licenses.started_at) as LicenseStartDate,
-        numbered_invoices.payment_platform as PaymentPlatform,
-        numbered_invoices.payer_email as PayerEmail,
+        last_licenses.license_type as LicenseType,
+        date(last_licenses.started_at) as LicenseStartDate,
+        last_invoices.payment_platform as PaymentPlatform,
+        last_invoices.payer_email as PayerEmail,
         users.n_total_creations as Geniallys,
 
     from users
-    left join numbered_licenses
-        on users.user_id = numbered_licenses.user_id
-    left join numbered_invoices
-        on numbered_licenses.subscription_id = numbered_invoices.subscription_id
-
-    -- We also check for null values here because we can have users
-    -- that have not paid for their access, such as genially employees :)
-    where (numbered_licenses.rn = 1 or numbered_licenses.rn is null)
-        and (numbered_invoices.rn = 1 or numbered_invoices.rn is null)
-    order by users.email
+    left join last_licenses
+        on users.user_id = last_licenses.user_id
+    left join last_invoices
+        on last_licenses.subscription_id = last_invoices.subscription_id
 )
 
 select * from final
