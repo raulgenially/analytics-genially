990c8fac0182cd26c3d61d83a7cb77360e75eaf0
LuisCebrian <luicebch@gmail.com>
2021-12-24 13:42:16 +0100

    #124 - Add a src model for the user snapshot model - Abstract the common logic to setup a users table into a macro

---
+++ macros/create_base_user_model.sql
@@ -0,0 +1,74 @@
+{% macro create_base_user_model(model=null, src=null) %}
+
+with users as (
+    select * from
+    {% if src %}
+        {{ source('genially', src) }}
+        where __hevo__marked_deleted = false
+            and email is not null
+    {% else %}
+        {{ ref(model) }}
+    {% endif %}
+),
+
+subscription_plans as (
+    select * from {{ ref('seed_plan') }}
+),
+
+-- There are some cases in which dateregister > lastaccesstime by a few seconds
+-- We assume these cases to be just a sync issue during the auth process.
+-- Here we fix these discrepancies by syncing lastaccesstime to dateregister if
+-- the time difference is behind a certain threshold.
+int_users as (
+    select
+        *,
+        -- We take dateregister as the true date
+        if(
+            dateregister > lastaccesstime
+            and abs(date_diff(dateregister, lastaccesstime, SECOND)) < 10,
+            dateregister,
+            lastaccesstime
+        ) as synced_lastaccesstime,
+    from users
+),
+
+final as (
+    select
+        *,
+        _id as user_id,
+
+        subscription_plans.plan as subscription_plan,
+        newsector as sector_code,
+        newrole as role_code,
+        lower(email) as email_lower,
+        {{ clean_country_code('country') }} as country_code,
+        -- socialmedia extraction
+        json_extract_scalar(socialmedia, '$.facebook') as facebook_account,
+        json_extract_scalar(socialmedia, '$.twitter') as twitter_account,
+        json_extract_scalar(socialmedia, '$.youtube') as youtube_account,
+        json_extract_scalar(socialmedia, '$.instagram') as instagram_account,
+        json_extract_scalar(socialmedia, '$.linkedin') as linkedin_account,
+        -- emailvalidationtoken extraction
+        json_extract_scalar(emailvalidationtoken, '$.Token') as email_validation_token,
+        summary as about_me,
+
+        ifnull(validated, False) as is_validated,
+
+        idanalytics as analytics_id,
+
+        -- First valid registration date is 2015-02-23T13:27:13 (as of 2021-07-15)
+        if(dateregister >= '2015-02-23', dateregister, null) as registered_at,
+        -- First valid last access date is 2016-06-02T17:01:47 (as of 2021-07-15)
+        if(synced_lastaccesstime >= '2016-06-02', synced_lastaccesstime, null) as last_access_at,
+        -- emailvalidationtoken extraction
+        timestamp_millis(cast(json_extract_scalar(emailvalidationtoken, '$.CreatedAt') as int64)) as email_validation_created_at,
+
+    from int_users
+    left join subscription_plans
+        on int_users.typesubscription = subscription_plans.code
+
+)
+
+select * from final
+
+{% endmacro %}
--- models/sources/genially/src_genially_users.sql
+++ models/sources/genially/src_genially_users.sql
@@ -1,69 +1,39 @@
 with users as (
-    select * from {{ source('genially', 'users') }}
-    where __hevo__marked_deleted = false
-        and email is not null
-),
-
-plans as (
-    select * from {{ ref('seed_plan') }}
-),
-
--- There are some cases in which dateregister > lastaccesstime by a few seconds
--- We assume these cases to be just a sync issue during the auth process.
--- Here we fix these discrepancies by syncing lastaccesstime to dateregister if
--- the time difference is behind a certain threshold.
-int_users as (
-    select
-        *,
-        -- We take dateregister as the true date
-        if(
-            dateregister > lastaccesstime
-            and abs(date_diff(dateregister, lastaccesstime, SECOND)) < 10,
-            dateregister,
-            lastaccesstime
-        ) as synced_lastaccesstime
-    from users
+    {{ create_base_user_model(src='users') }}
 ),
 
 final as (
     select
-        _id as user_id,
+        user_id,
 
-        plans.plan as subscription_plan,
-        newsector as sector_code,
-        newrole as role_code,
+        subscription_plan,
+        sector_code,
+        role_code,
         username,
         nickname,
-        lower(email) as email,
-        {{ clean_country_code('country') }} as country,
+        email_lower as email,
+        country_code as country,
         city,
         logins,
         language,
         organization,
-        -- socialmedia extraction
-        json_extract_scalar(socialmedia, '$.facebook') as facebook_account,
-        json_extract_scalar(socialmedia, '$.twitter') as twitter_account,
-        json_extract_scalar(socialmedia, '$.youtube') as youtube_account,
-        json_extract_scalar(socialmedia, '$.instagram') as instagram_account,
-        json_extract_scalar(socialmedia, '$.linkedin') as linkedin_account,
-        -- emailvalidationtoken extraction
-        json_extract_scalar(emailvalidationtoken, '$.Token') as email_validation_token,
-        summary as about_me,
+        facebook_account,
+        twitter_account,
+        youtube_account,
+        instagram_account,
+        linkedin_account,
+        email_validation_token,
+        about_me,
 
-        ifnull(validated, False) as is_validated,
+        is_validated,
 
-        idanalytics as analytics_id,
+        analytics_id,
 
-        -- First valid registration date is 2015-02-23T13:27:13 (as of 2021-07-15)
-        if(dateregister >= '2015-02-23', dateregister, null) as registered_at,
-        -- First valid last access date is 2016-06-02T17:01:47 (as of 2021-07-15)
-        if(synced_lastaccesstime >= '2016-06-02', synced_lastaccesstime, null) as last_access_at,
-        -- emailvalidationtoken extraction
-        timestamp_millis(cast(json_extract_scalar(emailvalidationtoken, '$.CreatedAt') as int64)) as email_validation_created_at,
+        registered_at,
+        last_access_at,
+        email_validation_created_at,
 
-    from int_users
-    left join plans
-        on int_users.typesubscription = plans.code
+    from users
 )
 
 select * from final
