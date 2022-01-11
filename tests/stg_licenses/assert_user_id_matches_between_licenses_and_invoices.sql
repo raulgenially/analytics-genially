c47437c45e52f639296abbacdabf25741711936c
Antonio Díaz Parra <antonio@genially.com>
2022-01-11 11:49:32 +0000

    #122 - Test user_id consistency

---
+++ tests/stg_licenses/assert_user_id_matches_between_licenses_and_invoices.sql
@@ -0,0 +1,27 @@
+-- The user id in licenses and invoices should match
+with licenses as (
+    select * from {{ ref('stg_licenses') }}
+),
+
+invoices as (
+    select * from {{ ref('stg_invoices') }}
+),
+
+final as (
+    select
+        licenses.license_id,
+        licenses.status,
+        licenses.user_id,
+        licenses.started_at,
+        invoices.invoice_id,
+        invoices.is_refund as invoice_is_refund,
+        invoices.user_id as invoice_user_id
+
+    from licenses
+    inner join invoices
+        on licenses.subscription_id = invoices.subscription_id
+    where licenses.user_id != invoices.user_id
+
+)
+
+select * from final
