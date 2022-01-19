3d22484cfc7f4e2d3cb4f2608361662a73e8b216
LuisCebrian <luicebch@gmail.com>
2022-01-19 11:19:56 +0100

    #XXXX - Fix the is_valid_euvat_number not_null test to account for     refunds from older invoices than the introduction of the tax     fields

---
+++ tests/src_genially_refund_invoices/assert_is_valid_euvat_number_is_not_null.sql
@@ -0,0 +1,24 @@
+-- is_valid_euvat_number should not be null for refunds that
+-- reference invoices created after var('invoice_tax_start_date')
+-- It can be null, however, if the invoice was created before that date.
+with refunds as (
+    select * from {{ ref('src_genially_refund_invoices') }}
+    where date(invoiced_at) > '{{ var('invoice_tax_start_date') }}'
+),
+
+invoices as (
+    select * from {{ ref('src_genially_invoices') }}
+),
+
+final as (
+    select
+        refunds.*
+
+    from refunds
+    inner join invoices
+        on refunds.reference_invoice_number = invoices.invoice_number
+    where refunds.is_valid_euvat_number is null
+        and invoices.invoiced_at > '{{ var('invoice_tax_start_date') }}'
+)
+
+select * from final
