4a441f3c3d0987fd68aadbd5fb7ceb5283db6021
LuisCebrian <luicebch@gmail.com>
2022-02-09 14:22:33 +0100

    #122 - Add an is_active flag to the licenses models

--- models/staging/stg_licenses.sql
+++ models/staging/stg_licenses.sql
@@ -6,15 +6,27 @@ canceled_licenses as (
     select * from {{ ref('src_genially_canceled_licenses') }}
 ),
 
+base_licenses as (
+    select
+        licenses.*,
+        {{
+            map_license_status(
+                'licenses.status',
+                'canceled_licenses.reason_code'
+           )
+        }} as subscription_status,
+        canceled_licenses.canceled_at
+
+    from licenses
+    left join canceled_licenses
+        on licenses.subscription_id = canceled_licenses.subscription_id
+),
+
 final as (
     select
         licenses.license_id,
 
-        {{ map_license_status(
-            'licenses.status',
-            'canceled_licenses.reason_code'
-           )
-        }} as status,
+        licenses.subscription_status as status,
         licenses.license_type,
         licenses.recurrence,
         licenses.plan,
@@ -24,11 +36,13 @@ final as (
         licenses.payer_id,
         licenses.subscription_id,
 
+        licenses.subscription_status in ('Active', 'Canceled') as is_active,
+
         licenses.started_at,
         licenses.finished_at,
-        canceled_licenses.canceled_at
+        licenses.canceled_at
 
-    from licenses
+    from base_licenses as licenses
     left join canceled_licenses
         on licenses.subscription_id = canceled_licenses.subscription_id
 )
