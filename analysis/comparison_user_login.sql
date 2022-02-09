efd557292637b0c9e13273141091ce5cb674a74d
Ana García Galán <anamaria@genially.com>
2022-02-09 16:49:03 +0000

    #161- Second comparison

--- analysis/comparison_user_login.sql
+++ analysis/comparison_user_login.sql
@@ -26,9 +26,34 @@ total_ga4 as (
         (hostname = 'auth.genial.ly' 
             and page_location='https://auth.genial.ly/es/onboarding'
             and page_referrer='https://app.genial.ly/'))
+),
+
+first_comparison as (
+    select user.comparison, total_user_login, total_ga4
+    from total_user_login user
+    left join total_ga4 ga4
+    on user.comparison=ga4.comparison
+),
+
+total_user_login_2 as (
+    select "2" as comparison, count(distinct user_id) as total_user_login
+    from  user_logins
+    WHERE date(login_at) = {{ date }}
+),
+
+total_ga4_2 as (
+    select "2" as comparison, count(distinct user_id) as total_ga4
+    from ga4_events
+    where date(event_at) = {{ date }}    
+),
+
+second_comparison as (
+    select user.comparison, total_user_login, total_ga4
+    from total_user_login_2 user
+    left join total_ga4_2 ga4
+    on user.comparison=ga4.comparison
 )
 
-select user.comparison, total_user_login, total_ga4
-from total_user_login user
-left join total_ga4 ga4
-on user.comparison=ga4.comparison
+select * from first_comparison
+union all
+select * from second_comparison
