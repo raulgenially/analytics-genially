38704d0b89b391c38a1bc4fecf6a0899ccd59c72
LuisCebrian <luicebch@gmail.com>
2022-02-18 13:16:57 +0100

    #168 - Add a model to describe the pan changes of the users

---
+++ models/marts/core/user_plan_history.sql
@@ -0,0 +1,47 @@
+with user_history as (
+    select
+        id,
+        user_id,
+        subscription_plan as plan,
+        state_valid_from as valid_from,
+
+    from {{ ref('src_snapshot_genially_users') }}
+),
+
+compare_next as (
+    select
+        id,
+        user_id,
+        plan,
+        lead(plan) over (
+            partition by user_id order by valid_from asc
+        ) as next_plan,
+        valid_from,
+
+    from user_history
+),
+
+final as (
+    select
+        id,
+
+        user_id,
+        plan,
+
+        valid_from,
+        ifnull(
+            timestamp_sub(
+                lead(valid_from) over (
+                    partition by user_id order by valid_from asc
+                ),
+                interval 1 second
+            ),
+            timestamp("{{ var('the_distant_future') }}")
+        ) as valid_to,
+
+    from compare_next
+    where plan != next_plan
+        or next_plan is null
+)
+
+select * from final
