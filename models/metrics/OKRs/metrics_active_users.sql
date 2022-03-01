d13b7f60e34dc97b495858a80376cfe351cd64e6
Ana García Galán <anamaria@genially.com>
2022-03-01 14:35:18 +0000

    #179 - metrics_active_users

---
+++ models/metrics/OKRs/metrics_active_users.sql
@@ -0,0 +1,38 @@
+{% set min_date_for_computations %}
+    date('2021-12-20')
+{% endset %}
+
+{% set max_date_for_computations %}
+    date('2022-12-31')
+{% endset %}
+
+{% set start_date_of_analysis %}
+    date('2022-01-01')
+{% endset %}
+
+with dates as (
+    {{ dbt_utils.date_spine(
+        datepart = "day",
+        start_date = min_date_for_computations,
+        end_date = max_date_for_computations
+        )
+    }}
+),
+
+active_users as (
+    select * from {{ ref('metrics_login_activity_active_users') }}
+),
+
+final as (
+    select 
+        dates.date_day as date_day,
+        sum(n_monthly_active_users) as active_users
+    
+    from dates
+    left join active_users
+        on date(active_users.date_day) = dates.date_day
+    where dates.date_day >= {{ start_date_of_analysis }}
+    group by 1
+)
+
+select * from final
