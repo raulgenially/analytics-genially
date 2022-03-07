594831c8e0942ad008e54a22c2766d5aa34a065a
Ana García Galán <anamaria@genially.com>
2022-03-07 13:46:57 +0100

    #179 - coalesce n_creations

--- models/metrics/login_activity/metrics_login_activity_active_users.sql
+++ models/metrics/login_activity/metrics_login_activity_active_users.sql
@@ -20,9 +20,9 @@ final as (
         countif(status_28d = 'New') as n_signups_28d,
         countif(status_28d = 'Returning') as n_returning_users_28d,
         countif(status_28d in ('New', 'Returning')) as n_monthly_active_users,
-        sum(n_creations) as n_creations,
-        sum(n_creations_7d) as n_weekly_creations,
-        sum(n_creations_28d) as n_monthly_creations
+        coalesce(sum(n_creations),0) as n_creations,
+        coalesce(sum(n_creations_7d),0) as n_weekly_creations,
+        coalesce(sum(n_creations_28d),0) as n_monthly_creations
 
     from activity
     {{ dbt_utils.group_by(n=11) }}
