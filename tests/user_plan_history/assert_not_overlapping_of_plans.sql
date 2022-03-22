567475620fc727782535c4a8f70af5bc8df1ec22
Ana García Galán <anamaria@genially.com>
2022-03-22 11:04:05 +0100

    #193 - indentation added [skip ci]

--- tests/user_plan_history/assert_not_overlapping_of_plans.sql
+++ tests/user_plan_history/assert_not_overlapping_of_plans.sql
@@ -5,15 +5,15 @@ with plan_history as (
 
 preceding_plan_values as (
     select
-    * ,
-    lag(plan) over (
-        partition by user_id
-        order by started_at asc
-    ) as preceding_plan,
-    lag(finished_at) over (
-        partition by user_id
-        order by started_at asc
-    ) as preceding_finished_at,
+        * ,
+        lag(plan) over (
+            partition by user_id
+            order by started_at asc
+        ) as preceding_plan,
+        lag(finished_at) over (
+            partition by user_id
+            order by started_at asc
+        ) as preceding_finished_at,
 
     from plan_history
 ),
