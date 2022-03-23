f8240fb045fdc001e5495e809cc682f6f01b72f1
Ana García Galán <anamaria@genially.com>
2022-03-23 15:23:51 +0100

    #194 - preceding_plan deleted [skip ci]

--- tests/user_plan_history/assert_not_overlapping_of_plans.sql
+++ tests/user_plan_history/assert_not_overlapping_of_plans.sql
@@ -6,14 +6,10 @@ with plan_history as (
 preceding_plan_values as (
     select
         * ,
-        lag(plan) over (
-            partition by user_id
-            order by started_at asc
-        ) as preceding_plan,
         lag(finished_at) over (
             partition by user_id
             order by started_at asc
-        ) as preceding_finished_at,
+        ) as preceding_finished_at
 
     from plan_history
 ),
