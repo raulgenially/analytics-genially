8c81e16fa9a3652252fc6d9bde58743a3657dc01
raulgenially <raul@genial.ly>
2022-04-06 15:53:13 +0200

    #217 - Change platform name [skip ci]

--- macros/map_payment_platform.sql
+++ macros/map_payment_platform.sql
@@ -5,7 +5,7 @@
             then 'Stripe'
         when {{ realtransactionid }} like 'py\\_%'
             -- example: py_3KVcV6Bn82mIxvX23opSBFGI
-            then 'Stripe_PayPal'
+            then 'Stripe-PayPal'
         when {{ realtransactionid }} like '%-%-%-%-%'
             -- example: 4115d837-f87b-49dd-a1ab-8e6c8aa842ff
             or {{ realtransactionid }} like 'PP-D-%'
