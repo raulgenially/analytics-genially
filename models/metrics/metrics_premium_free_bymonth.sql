95d00056698ff10f029bf9a6bc8f7427852d44b8
LuisCebrian <luicebch@gmail.com>
2022-04-12 10:05:20 +0200

    #214 - model created and okr model changed

---
+++ models/metrics/metrics_premium_free_bymonth.sql
@@ -0,0 +1,39 @@
+{% set start_date_of_analysis %}
+    '{{ var('start_date_of_analysis_OKR') }}'
+{% endset %}
+
+with dates as (
+    {{ dbt_utils.date_spine(
+        datepart="month",
+        start_date=start_date_of_analysis,
+        end_date="current_date()"
+        )
+    }}
+),
+
+metric_lastday_of_the_month as (
+    select
+        *,
+        date_trunc(date_day, month) as date_month
+
+    from {{ ref('metrics_okrs_premium_free') }}
+    where date_day = last_day(date_day,month)
+),
+
+final as (
+    select
+        date(dates.date_month) as date_month,
+        m.plan,
+        m.country,
+        m.country_name,
+        m.broad_sector,
+        m.broad_role,
+        m.free_users,
+        m.premium_users
+
+    from dates
+    left join metric_lastday_of_the_month as m
+        on date(dates.date_month)=m.date_month
+)
+
+select * from final
