8d87025a6381259c92b4ca66a03a90beb1bf3090
manaji <mariajim22@gmail.com>
2022-05-26 09:07:49 +0200

    #273 - Update drop_audit_dataset macro to work only in prod environment

--- macros/operations/drop_audit_dataset.sql
+++ macros/operations/drop_audit_dataset.sql
@@ -1,14 +1,10 @@
 {% macro drop_audit_dataset(dry_run=False) %}
-    {% set audit_dataset -%}
-        {{ 'dbt_test__audit' if target.name == 'prod' else target.dataset }}
-    {%- endset %}
-
     {% set drop_command -%}
-        drop schema if exists `{{ target.database }}.{{ audit_dataset }}` cascade;
+        drop schema if exists `{{ target.database }}.dbt_test__audit` cascade;
     {%- endset %}
 
     {% do log(drop_command, True) %}
-    {% if dry_run | as_bool == False %}
+    {% if dry_run | as_bool == False and target.name == 'prod' %}
         {% do run_query(drop_command) %}
     {% endif %}
 
