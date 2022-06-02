b355602f98b620b788423b66f58129bad75de0e1
manaji <mariajim22@gmail.com>
2022-06-02 12:55:25 +0200

    #276 - Change target dataset and move transformations to source model

--- models/marts/bq_logs/bq_logs.sql
+++ models/marts/bq_logs/bq_logs.sql
@@ -4,58 +4,12 @@
             "field": "create_time",
             "data_type": "timestamp",
             "granularity": "day"
-            },
-        verbose=True
+            }
     )
 }}
 
 with bq_logs as (
     select * from {{ ref('src_bq_logs') }}
-),
-
-int_bq_logs as (
-    select
-        bq_logs_id,
-        query,
-        case
-            when regexp_contains(query, r'(?s)^SELECT \* FROM \(\nSELECT clmn[0-9_]+.*LIMIT 20000000|^SELECT t0\..*t0 LIMIT 100;|^SELECT t0[\._].* AS t0_.* LIMIT 100;')
-                then "Data Studio"
-            when (regexp_contains(query, r'^\/\* {"app": "dbt"')
-                    or principal_email like '%dbt-analytics-prod%'
-                    or principal_email like '%dbt-analytics-ci%')
-                then "dbt"
-            when (regexp_contains(query, r'.*__HEVO_QUERY_.*')
-                    or principal_email like "%hevodata%")
-                then "Hevo"
-            when regexp_contains(query, r'^-- Metabase.*')
-                then "Metabase"
-            else "Other"
-        end as query_source,
-        principal_email,
-        referenced_tables,
-
-        coalesce(
-            safe_divide(
-                total_slots_ms,
-                timestamp_diff(end_time, start_time, millisecond)
-            )
-        ) as avg_slots,
-        total_processed_bytes,
-        (total_billed_bytes / pow(2,30)) as total_billed_gigabytes,
-        (total_billed_bytes / pow(2,40)) * 5 as estimated_cost_usd,
-
-        coalesce(total_processed_bytes, total_slots_ms, error_code) is null
-            as is_cached,
-        coalesce(
-            regexp_contains(query, 'cloudaudit_googleapis_com_data_access_'),
-            false
-        ) as is_audit_dashboard_query,
-        error_code is not null as is_error,
-
-        timestamp_diff(end_time, start_time, second) as runtime_secs,
-        create_time
-
-    from bq_logs
 ),
 
 final as (
@@ -75,7 +29,7 @@ final as (
         runtime_secs,
         create_time
 
-    from int_bq_logs
+    from bq_logs
 )
 
 select * from final
--- models/sources/bq_logs/src_bq_logs.sql
+++ models/sources/bq_logs/src_bq_logs.sql
@@ -9,17 +9,19 @@
     )
 {% endset %}
 {% set months = 12 %}
+{% set price_per_tb = 5 %}
 
-with base_bq_logs as (
+
+with raw_bq_logs as (
     select * from {{ source('bq_logs', 'cloudaudit_googleapis_com_data_access') }}
 ),
 
 -- Here we remove rows when insertId column is duplicated
 bq_logs as (
-    {{ unique_records_by_column('base_bq_logs', 'insertId') }}
+    {{ unique_records_by_column('raw_bq_logs', 'insertId') }}
 ),
 
-int_bq_logs as (
+base_bq_logs as (
     select
         insertId as bq_logs_id,
         protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobConfiguration.query.query as query,
@@ -50,18 +52,80 @@ int_bq_logs as (
 
 ),
 
+int_bq_logs as (
+    select
+        bq_logs_id,
+        query,
+        case
+            when regexp_contains(query, r'(?s)^SELECT \* FROM \(\nSELECT clmn[0-9_]+.*LIMIT 20000000|^SELECT t0\..*t0 LIMIT 100;|^SELECT t0[\._].* AS t0_.* LIMIT 100;')
+                then "Data Studio"
+            when (regexp_contains(query, r'^\/\* {"app": "dbt"')
+                    or principal_email like '%dbt-analytics-prod%'
+                    or principal_email like '%dbt-analytics-ci%')
+                then "dbt"
+            when (regexp_contains(query, r'.*__HEVO_QUERY_.*')
+                    or principal_email like "%hevodata%")
+                then "Hevo"
+            when regexp_contains(query, r'^-- Metabase.*')
+                then "Metabase"
+            else "Other"
+        end as query_source,
+        principal_email,
+        referenced_tables,
+
+        error_code,
+        total_slots_ms,
+        safe_divide(
+            total_slots_ms,
+            timestamp_diff(end_time, start_time, millisecond)
+        ) as avg_slots,
+
+        total_processed_bytes,
+        total_billed_bytes,
+        (total_billed_bytes / pow(2,30)) as total_billed_gigabytes,
+        (total_billed_bytes / pow(2,40)) * {{ price_per_tb }}
+            as estimated_cost_usd,
+
+        coalesce(total_processed_bytes, total_slots_ms, error_code) is null
+            as is_cached,
+        coalesce(
+            regexp_contains(query, 'cloudaudit_googleapis_com_data_access_'),
+            false
+        ) as is_audit_dashboard_query,
+        error_code is not null as is_error,
+
+        start_time,
+        end_time,
+        timestamp_diff(end_time, start_time, second) as runtime_secs,
+        create_time
+
+    from base_bq_logs
+),
+
 final as (
     select
         bq_logs_id,
         query,
+        query_source,
         principal_email,
         referenced_tables,
+
         error_code,
         total_slots_ms,
+        avg_slots,
+
         total_processed_bytes,
         total_billed_bytes,
+        total_billed_gigabytes,
+        estimated_cost_usd,
+
+        is_cached,
+        is_audit_dashboard_query,
+        is_error,
+
         start_time,
         end_time,
+        runtime_secs,
         create_time
 
     from int_bq_logs
